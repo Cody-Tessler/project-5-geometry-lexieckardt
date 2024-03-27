2c8fe29be537ef8726a5ecb86114c445899d8fd5
lexieckardt <156743839+lexieckardt@users.noreply.github.com>
2024-03-27 19:28:25 +0000

    Circle code completed

--- circle.py
+++ circle.py
@@ -3,21 +3,47 @@ from shape import Shape
 class Circle(Shape):
 
     def __init__(self, r):
-        ...
+        """
+        Constructs a Circle object
+
+        :param c: float -> radius of circle
+        """
+        self.check_if_args_not_below_zero(r)
+        self.r = r
 
     def get_area(self):
-        return 0
+        """
+        Calculates Circle area using formula (pi)(r ** 2)
+        
+        :return: float -> area of triangle
+        """
+        return math.pi * (self.r ** 2)
 
     def get_perimeter(self):
-        return 0
+        """
+        Calculates Circle perimeter using formula 2(pi)(r)
+        
+        :return: float -> perimeter of triangle
+        """
+        return 2 * math.pi * self.r
 
     def __str__(self):
-        return "TODO"
+        return f"Circle,  r={self.r} "
 
     @classmethod
     def get_area_formula(cls):
-        return "TODO"
+        """
+        Returns area formula for Circle as a string.
+        
+        :return: string
+        """
+        return "pi(r**2)"
 
     @classmethod
     def get_perimeter_formula(cls):
-        return "TODO"
+        """
+        Returns perimeter formula for Circle as a string.
+        
+        :return: string
+        """
+        return "2 * pi * r"
